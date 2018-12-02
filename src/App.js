f05a1701af9d6068d3060f5ad15fdc79ee0bc813
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-12-01 22:39:42 -0800

    added displays for summer and winter based on current location

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     lat: null,
+  //     long: null,
+  //     errorMessage: ''
+  //   };
+  // }
+
+  state = {
+    lat: null,
+    long: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude, long: position.coords.longitude }),
+      err => this.setState({ errorMessage: err.message })
     );
+  };
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered!')
+  }
+
+  render() {
+
+    if (this.state.errorMessage && (!this.state.lat || !this.state.long)) {
+      return <div className="App"><div>Error: {this.state.errorMessage}</div></div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat && this.state.long) {
+      // return <div className="App"><div>My Location:</div><div>Latitude: {this.state.lat}</div> <div>Longitude: {this.state.long}</div></div>
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+
+    return <div className="App"><div>Loading!</div></div>
+
+    // return (
+    //   <div className="App">
+    //     <div>My current location:</div>
+    //     {this.state.errorMessage ? <div>Error: {this.state.errorMessage}</div> : null}
+    //     {!this.state.errorMessage ? <div>Latitude: {this.state.lat} Longitude: {this.state.long}</div> : null}
+
+    //     <SeasonDisplay />
+    //   </div>
+    // );
   }
 }
 
